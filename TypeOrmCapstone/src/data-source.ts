0bcfe663c030c984a5072b6f6c95831905910af6
FoxStrike <denriquedc@gmail.com>
2023-11-13 15:49:59 -0600

    server json

--- TypeOrmCapstone/src/data-source.ts
+++ TypeOrmCapstone/src/data-source.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
+import { Docs } from "./entity/Docs"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -11,7 +12,7 @@ export const AppDataSource = new DataSource({
     database: "academicaidedb",
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [User,Docs],
     migrations: [],
     subscribers: [],
 })
