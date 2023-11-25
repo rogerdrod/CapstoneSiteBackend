ca9460dd9f289a12b5b352399e8b9b9d12d5839c
FoxStrike <denriquedc@gmail.com>
2023-11-24 18:21:24 -0600

    login and status changes

--- TypeOrmCapstone/src/index.ts
+++ TypeOrmCapstone/src/index.ts
@@ -27,6 +27,27 @@ AppDataSource.initialize()
     console.log("Here you can set up and run express / fastify / any other framework.");
 
     // Register routes
+    app.get("/user/status/:email", async (req: express.Request, res: express.Response) => {
+      try {
+        const email = req.params.email;
+        
+        // Fetch the user from the database based on the email
+        const userRepository = AppDataSource.getRepository(User);
+        const user = await userRepository.findOne({
+          where: { email: email }
+        });
+    
+        if (!user) {
+          return res.status(404).json({ error: "User not found" });
+        }
+    
+        // Respond with the user's status
+        return res.status(200).json({ status: user.status });
+      } catch (error) {
+        console.error("Error fetching user status:", error);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
 
     app.get("/user/all", async function (req: express.Request, res: express.Response) {
       const users = await AppDataSource.getRepository(User).find();
